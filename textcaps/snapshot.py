490b7303f0a9cbfa7eb19e95532038c932761e8e
Sujit Sundar Amin <143650989+sujitaminusc@users.noreply.github.com>
2023-12-06 20:53:01 -0800

    snapshot.py

--- textcaps/snapshot.py
+++ textcaps/snapshot.py
@@ -7,13 +7,26 @@ class SnapshotModelCheckpoint(Callback):
     def __init__(self, nb_epochs, nb_snapshots, fn_prefix='Model'):
         super(SnapshotModelCheckpoint, self).__init__()
 
-        self.check = nb_epochs // nb_snapshots
+        self.check = self.calculate_check_interval(nb_epochs, nb_snapshots)
         self.fn_prefix = fn_prefix
 
+    def calculate_check_interval(self, nb_epochs, nb_snapshots):
+        return nb_epochs // nb_snapshots
+
     def on_epoch_end(self, epoch, logs={}):
-        if epoch != 0 and (epoch + 1) % self.check == 0:
-            filepath = self.fn_prefix + "-%d.h5" % ((epoch + 1) // self.check)
-            self.model.save(filepath, overwrite=True)
+        if self.should_save_snapshot(epoch):
+            filepath = self.generate_filepath(epoch)
+            self.save_model(filepath)
+
+    def should_save_snapshot(self, epoch):
+        return epoch != 0 and (epoch + 1) % self.check == 0
+
+    def generate_filepath(self, epoch):
+        snapshot_number = (epoch + 1) // self.check
+        return f"{self.fn_prefix}-{snapshot_number}.h5"
+
+    def save_model(self, filepath):
+        self.model.save(filepath, overwrite=True)
 
 
 class SnapshotCallbackBuilder:
@@ -23,19 +36,32 @@ class SnapshotCallbackBuilder:
         self.alpha_zero = init_lr
         self.save_dir = save_dir
 
-    def get_callbacks(self,log, model_prefix='Model'):
-        if not os.path.exists(self.save_dir+'/weights/'):
-            os.makedirs(self.save_dir+'/weights/')
+    def get_callbacks(self, log, model_prefix='Model'):
+        self._create_save_directory()
 
-        callback_list = [callbacks.ModelCheckpoint(self.save_dir+"/weights/weights_{epoch:002d}.h5", monitor="val_capsnet_acc",
-                                                    save_best_only=True, save_weights_only=False),
-                         callbacks.LearningRateScheduler(schedule=self._cosine_anneal_schedule),
-                         SnapshotModelCheckpoint(self.T, self.M, fn_prefix=self.save_dir+'/weights/%s' % model_prefix), log]
+        callback_list = [
+            callbacks.ModelCheckpoint(
+                self.save_dir + "/weights/weights_{epoch:002d}.h5",
+                monitor="val_capsnet_acc",
+                save_best_only=True,
+                save_weights_only=False
+            ),
+            callbacks.LearningRateScheduler(schedule=self._cosine_anneal_schedule),
+            SnapshotModelCheckpoint(self.T, self.M, fn_prefix=self.save_dir + '/weights/%s' % model_prefix),
+            log
+        ]
 
         return callback_list
 
+    def _create_save_directory(self):
+        if not os.path.exists(self.save_dir + '/weights/'):
+            os.makedirs(self.save_dir + '/weights/')
+
+
     def _cosine_anneal_schedule(self, t):
-        cos_inner = np.pi * (t % (self.T // self.M))  # t - 1 is used when t has 1-based indexing.
-        cos_inner /= self.T // self.M
+        cos_inner = self._calculate_cosine_inner(t)
         cos_out = np.cos(cos_inner) + 1
         return float(self.alpha_zero / 2 * cos_out)
+
+    def _calculate_cosine_inner(self, t):
+        return np.pi * (t % (self.T // self.M)) / (self.T // self.M)
